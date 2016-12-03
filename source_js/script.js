bdf911b28a861413a2e6bab7d3d51f0b55e6aa16
Neha Sharma <Sharma@wirelessprv-10-194-118-43.near.illinois.edu>
2016-12-03 16:44:48 -0600

    blah

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,26 @@
-// Write any custom javascript functions here
+$(document).ready(function() {
+
+	$('.form-control').on('focus blur', function (e) {
+			$(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+	}).trigger('blur');
+
+	$('#moveleft').click(function() {
+  		$('#textbox').animate({
+			'marginLeft': "0" //moves left
+  		});
+
+  		$('.toplam').animate({
+			'marginLeft': "100%" //moves right
+  		});
+	});
+
+	$('#moveright').click(function() {
+  		$('#textbox').animate({
+			'marginLeft': "50%" //moves right
+  		});
+
+  		$('.toplam').animate({
+			'marginLeft': "0" //moves right
+  		});
+	});
+});
