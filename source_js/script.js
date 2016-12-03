84790e2439fb6822cf12b3cd57164432b06e3263
Neha Sharma <Sharma@wirelessprv-10-194-118-43.near.illinois.edu>
2016-12-03 17:13:00 -0600

    shhh

--- source_js/script.js
+++ source_js/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 
 	$('.form-control').on('focus blur', function (e) {
-			$(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+    	$(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
 	}).trigger('blur');
 
 	$('#moveleft').click(function() {
   		$('#textbox').animate({
-			'marginLeft': "0" //moves left
+    		'marginLeft': "0" //moves left
   		});
 
   		$('.toplam').animate({
-			'marginLeft': "100%" //moves right
+    		'marginLeft': "100%" //moves right
   		});
 	});
 
 	$('#moveright').click(function() {
   		$('#textbox').animate({
-			'marginLeft': "50%" //moves right
+    		'marginLeft': "50%" //moves right
   		});
 
   		$('.toplam').animate({
-			'marginLeft': "0" //moves right
+    		'marginLeft': "0" //moves right
   		});
 	});
 });
