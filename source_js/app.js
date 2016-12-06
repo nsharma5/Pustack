e2532a5a521644c6ff7b27f093ec16db25c662ee
Shivam Gupta <shivamvmc@gmail.com>
2016-12-05 23:54:44 -0600

    Update app.js

--- source_js/app.js
+++ source_js/app.js
@@ -6,7 +6,7 @@ app.config(function ($routeProvider, $locationProvider) {
 			templateUrl:'partials/landing.html',
 			controller: 'landingController'
 		})
-		.when ('/lectures',{
+		.when ('/lectures/:selectedSubjectId',{
 			templateUrl:'partials/lectures.html',
 			controller: 'lectureController'
 		})
